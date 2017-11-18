23faccf5895c231dd6d4190ec9c8e4ce80ed3a1a
吉田 祥子 <shoko@yoshida-shouko-no-imac.local>
2017-11-19 01:54:25 +0900

    参院の質問主意書変換機能追加。が、動かない。。PDFの書式に問題があるのかもしれない。

---
+++ sanin_QandA.py
@@ -0,0 +1,104 @@
+# 参考 
+# https://qiita.com/Taillook/items/a0f2c59d8e17381fc835
+# https://media.accel-brain.com/web-pdf-abstract-academic-papers/
+# 質問主意書のタイトルは文字化けします。
+
+import requests
+import re
+import urllib
+from html.parser import HTMLParser
+from argparse import ArgumentParser
+
+class ParserTable(HTMLParser):
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.data = []
+		self.title = False
+		self.syup = False
+		self.toup = False
+	
+	def handle_starttag(self, tag, attrs):
+		attrs = dict(attrs)
+
+		if tag == "a" and re.match("meisai", attrs["href"]):
+			self.title = True
+			self.data.append({})
+
+		elif tag == "a" and re.match("syup", attrs["href"]):
+			self.data[-1].update({"syup":attrs["href"]})
+			self.syup = False
+
+		elif self.syup == True:
+			self.data[-1].update({"syup":"not yet"})
+			self.syup = False
+
+		elif tag == "a" and re.match("toup", attrs["href"]):
+			self.data[-1].update({"toup":attrs["href"]})
+			self.toup = False
+
+		elif self.toup == True:
+			self.data[-1].update({"toup":"not yet"})
+			self.toup = False
+
+	def handle_data(self, data):
+		if self.title == True:
+			self.data[-1].update({"title":data})
+			self.title = False
+			self.syup = True
+			self.toup = True
+
+# 参照する国会会期のURLを指定 
+f = requests.get("http://www.sangiin.go.jp/japanese/joho1/kousei/syuisyo/195/syuisyo.htm")
+
+parser = ParserTable()
+parser.feed(f.text)
+parser.close()
+f.close()
+
+# PDFファイルのルート 
+root_pdf = "http://www.sangiin.go.jp/japanese/joho1/kousei/syuisyo/195/"
+
+# PDFファイルダウンロード先
+dir_name = "sanin_downloads"
+
+# コマンドライン引数の設定 
+arg_parser = ArgumentParser()
+arg_parser.add_argument("--number", "-n", help="specify question number",type=int)
+arg_parser.add_argument("--list","-l", action="store_true", help="list all the quiestions")
+arg_parser.add_argument("--download", "-d", help="download q and a", type=int) 
+args = arg_parser.parse_args()
+
+if args.number:
+	q = args.number - 1
+	if q >= 0 and q < len(parser.data):
+		print(parser.data[q]["title"])
+		print(root_pdf + parser.data[q]["syup"])
+		print(root_pdf + parser.data[q]["toup"])
+
+if args.list:
+	i = 0
+	for d in parser.data:
+		print(d["title"])
+
+		if d["syup"] != "not yet":
+			print(root_pdf + d["syup"])
+		else:
+			print(d["syup"])
+	
+		if d["toup"] != "not yet":
+			print(root_pdf + d["toup"])
+		else:
+			print(d["toup"])
+
+		print("")
+
+if args.download:
+	if parser.data[args.download-1]["syup"] != "not yet":
+		result = urllib.request.urlretrieve(root_pdf + parser.data[args.download-1]["syup"], dir_name + "/q_" + str(args.download) + ".pdf")
+	if parser.data[args.download-1]["toup"] != "not yet":
+		result = urllib.request.urlretrieve(root_pdf + parser.data[args.download-1]["toup"], dir_name + "/a_" + str(args.download) + ".pdf")
+	print(parser.data[args.download-1]["title"])
+	print(root_pdf + parser.data[args.download-1]["syup"])
+	print(root_pdf + parser.data[args.download-1]["toup"])
+
+
--- test_QandA.sh
+++ test_QandA.sh
@@ -1,10 +1,38 @@
 #!/bin/sh
-PDF_GET="/Users/shoko/Develop/myscripts/shuin_QandA.py"
+
+PYTHON=python3
+SHU_PDF_GET="/Users/shoko/Develop/myscripts/shuin_QandA.py"
+SAN_PDF_GET="/Users/shoko/Develop/myscripts/sanin_QandA.py"
+SHU_PDF_DIR="/Users/shoko/Develop/myscripts/shuin_downloads/"
+SAN_PDF_DIR="/Users/shoko/Develop/myscripts/sanin_downloads/"
+
 PDF_CONV="/Users/shoko/Develop/pdfminer.six/tools/pdf2txt.py"
-PDF_DIR="/Users/shoko/Develop/myscripts/shuin_downloads/"
 TXT="/Users/shoko/Downloads/q_and_a.txt"
 
-python3 ${PDF_GET} -d $1 > ${TXT} 
-python3 ${PDF_CONV} -V "${PDF_DIR}q_$1.pdf" >> ${TXT}
-python3 ${PDF_CONV} -V "${PDF_DIR}a_$1.pdf" >> ${TXT}
+ARG_TYPE=$1
+ARG_QNUM=$2
+if [ ${ARG_TYPE} == "shu" ]; then
 
+	${PYTHON} ${SHU_PDF_GET} -d ${ARG_QNUM} > ${TXT} 
+
+	if [ -f "${SHU_PDF_DIR}q_${ARG_QNUM}.pdf" ]; then
+		${PYTHON} ${PDF_CONV} -V "${SHU_PDF_DIR}q_${ARG_QNUM}.pdf" >> ${TXT}
+	fi
+	if [ -f "${SHU_PDF_DIR}a_${ARG_ARG_QNUM}.pdf" ]; then
+		${PYTHON} ${PDF_CONV} -V "${SHU_PDF_DIR}a_${ARG_QNUM}.pdf" >> ${TXT}
+	fi
+
+elif [ ${ARG_TYPE} = "san" ]; then
+	${PYTHON} ${SAN_PDF_GET} -d ${ARG_QNUM} #> ${TXT} 
+	
+	if [ -f "${SAN_PDF_DIR}q_${ARG_QNUM}.pdf" ]; then
+		echo "${SAN_PDF_DIR}q_${ARG_QNUM}.pdf"
+		${PYTHON} ${PDF_CONV} -V "${SAN_PDF_DIR}q_${ARG_QNUM}.pdf" #>> ${TXT}
+	fi
+	if [ -f "${SAN_PDF_DIR}a_${ARG_ARG_QNUM}.pdf" ]; then
+		${PYTHON} ${PDF_CONV} -V "${SAN_PDF_DIR}a_${ARG_QNUM}.pdf" #>> ${TXT}
+	fi
+
+else
+	echo "error"
+fi
