0b2b19261a53842c15ee2e406b4a6f62d9a95194
吉田 祥子 <shoko@yoshida-shouko-no-imac.local>
2017-11-24 18:03:56 +0900

    変換文字追加＆設定ファイル更新

--- conv_QandA.py
+++ conv_QandA.py
@@ -17,15 +17,22 @@ if args.file:
         ord('﹁'):'「',
         ord('﹂'):'」',
         ord('︵'):'（',
-        ord('︶'):'）'
+        ord('︶'):'）',
+        ord('〇'):'0'
     })
 
     src = src.replace("(cid:7923)","っ")
     src = src.replace("(cid:7939)","ヶ")
     src = src.replace("(cid:7891)","ー")
+    src = src.replace("(cid:7931)","ェ")
+    src = src.replace("(cid:7934)","ャ")
     src = src.replace("(cid:7935)","ュ")
+    src = src.replace("(cid:7936)","ョ")
     src = src.replace("(cid:7933)","ッ")
     src = src.replace("(cid:7929)","ィ")
+    src = src.replace("(cid:7722)","遡")
+    src = src.replace("(cid:7970)","餐")
+    src = src.replace("(cid:7894)","〜")
 
     src = src.replace("平成三十年","2018/")
     src = src.replace("平成二十九年","2017/")
@@ -34,8 +41,27 @@ if args.file:
     src = src.replace("十一月","11/")
     src = src.replace("十二月","12/")
     src = src.replace("十日","10")
+    src = src.replace("十一日","11")
+    src = src.replace("十二日","12")
+    src = src.replace("十三日","13")
+    src = src.replace("十四日","14")
+    src = src.replace("十五日","15")
+    src = src.replace("十六日","16")
+    src = src.replace("十七日","17")
+    src = src.replace("十八日","18")
+    src = src.replace("十九日","19")
     src = src.replace("二十日","20")
-    src = src.replace("三十日","20")
+    src = src.replace("二十一日","21")
+    src = src.replace("二十二日","22")
+    src = src.replace("二十三日","23")
+    src = src.replace("二十四日","24")
+    src = src.replace("二十五日","25")
+    src = src.replace("二十六日","26")
+    src = src.replace("二十七日","27")
+    src = src.replace("二十八日","28")
+    src = src.replace("二十九日","29")
+    src = src.replace("三十日","30")
+    src = src.replace("三十一日","31")
 
     src = src.replace("\n一","１.")
     src = src.replace("\n二","２.")
@@ -53,7 +79,7 @@ if args.file:
     conv = src.maketrans("一二三四五六七八九","123456789")
     src = src.translate(conv)
 
-    src = src.replace("十","")
+    src = src.replace("十","10")
     src = src.replace("年","/")
     src = src.replace("月","/")
     src = src.replace("日","")
