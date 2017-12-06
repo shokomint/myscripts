fe488f41edd6ffb6fd9a6c8982b013d5d53c16bc
吉田 祥子 <shoko@yoshida-shouko-no-imac.local>
2017-12-06 20:11:08 +0900

    指定した質問主意書を取得する

--- sanin_QandA.py
+++ sanin_QandA.py
@@ -10,54 +10,72 @@ from argparse import ArgumentParser
 from kokkailib import LogDone,UndoneList
 
 class ParserTable(HTMLParser):
-	def __init__(self):
-		HTMLParser.__init__(self)
-		self.data = []
-		self.title = False
-		self.syuh = False
-		self.touh = False
-	
-	def handle_starttag(self, tag, attrs):
-		attrs = dict(attrs)
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.data = []
+        self.title = False
+        self.syuh = False
+        self.touh = False
+    
+    def handle_starttag(self, tag, attrs):
+        attrs = dict(attrs)
 
-		if tag == "a" and re.match("meisai", attrs["href"]):
-			self.title = True
-			self.data.append({})
+        if tag == "a" and re.match("meisai", attrs["href"]):
+            self.title = True
+            self.data.append({})
 
-		elif tag == "a" and re.match("syuh", attrs["href"]):
-			self.data[-1].update({"syuh":attrs["href"]})
-			self.syuh = False
+        elif tag == "a" and re.match("syuh", attrs["href"]):
+            self.data[-1].update({"syuh":attrs["href"]})
+            self.syuh = False
 
-		elif self.syuh == True:
-			self.data[-1].update({"syuh":"not yet"})
-			self.syuh = False
+        elif self.syuh == True:
+            self.data[-1].update({"syuh":"not yet"})
+            self.syuh = False
 
-		elif tag == "a" and re.match("touh", attrs["href"]):
-			self.data[-1].update({"touh":attrs["href"]})
-			self.touh = False
+        elif tag == "a" and re.match("touh", attrs["href"]):
+            self.data[-1].update({"touh":attrs["href"]})
+            self.touh = False
 
-		elif self.touh == True:
-			self.data[-1].update({"touh":"not yet"})
-			self.touh = False
+        elif self.touh == True:
+            self.data[-1].update({"touh":"not yet"})
+            self.touh = False
 
-	def handle_data(self, data):
-		if self.title == True:
-			self.data[-1].update({"title":data})
-			self.title = False
-			self.syuh = True
-			self.touh = True
+    def handle_data(self, data):
+        if self.title == True:
+            self.data[-1].update({"title":data})
+            self.title = False
+            self.syuh = True
+            self.touh = True
 
-	def get_title(self, number):
-		return(self.data[number]["title"])
+    def get_title(self, number):
+        return(self.data[number]["title"])
 
-	def get_slink(self, number):
-		return(self.data[number]["syuh"])
+    def get_slink(self, number):
+        return(self.data[number]["syuh"])
 
-	def get_tlink(self, number):
-		return(self.data[number]["touh"])
+    def get_tlink(self, number):
+        return(self.data[number]["touh"])
 
-	def get_len(self):
-		return(len(self.data))
+    def get_len(self):
+        return(len(self.data))
+
+class ParserContents(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.data = ""
+        self.flg = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "td":
+            self.flg = True
+
+    def handle_endtag(self, tag):
+        if tag == "td":
+            self.flg = False
+
+    def handle_data(self, data):
+        if self.flg == True:
+            self.data += data
 
 # 参照する国会会期のURLを指定 
 f = requests.get("http://www.sangiin.go.jp/japanese/joho1/kousei/syuisyo/195/syuisyo.htm")
@@ -84,45 +102,53 @@ arg_parser.add_argument("--ulist","-ul", action="store_true",help="list undone l
 args = arg_parser.parse_args()
 
 if args.number:
-	q = args.number - 1
-	if q >= 0 and q < parser.get_len():
-		print(parser.get_title(q))
-		print(root_html + parser.get_slink(q))
-		print(root_html + parser.get_tlink(q))
+    q = args.number - 1
+    if q >= 0 and q < parser.get_len():
+        print(parser.get_title(q))
+        print(root_html + parser.get_slink(q))
+        print(root_html + parser.get_tlink(q))
+
+        f = requests.get(root_html + parser.get_slink(q))
+        f.encoding = "utf-8"
+        p_contents = ParserContents()
+        p_contents.feed(f.text)
+        p_contents.close()
+        f.close()
+        print(p_contents.data)
 
 if args.list:
-	i = 0
-	for d in parser.data:
-		print(d["title"])
+    i = 0
+    for d in parser.data:
+        print(d["title"])
 
-		if d["syuh"] != "not yet":
-			print(root_html + d["syuh"])
-		else:
-			print(d["syuh"])
-	
-		if d["touh"] != "not yet":
-			print(root_html + d["touh"])
-		else:
-			print(d["touh"])
+        if d["syuh"] != "not yet":
+            print(root_html + d["syuh"])
+        else:
+            print(d["syuh"])
+    
+        if d["touh"] != "not yet":
+            print(root_html + d["touh"])
+        else:
+            print(d["touh"])
 
-		print("")
+        print("")
 
 elif args.download:
-	if parser.data[args.download-1]["syuh"] != "not yet":
-		result = urllib.request.urlretrieve(root_html + parser.data[args.download-1]["syuh"], dir_name + "/q_" + str(args.download) + ".htm")
-	if parser.data[args.download-1]["touh"] != "not yet":
-		result = urllib.request.urlretrieve(root_html + parser.data[args.download-1]["touh"], dir_name + "/a_" + str(args.download) + ".htm")
-	print(parser.data[args.download-1]["title"])
-	print(root_html + parser.data[args.download-1]["syuh"])
-	print(root_html + parser.data[args.download-1]["touh"])
+    if parser.data[args.download-1]["syuh"] != "not yet":
+        result = urllib.request.urlretrieve(root_html + parser.data[args.download-1]["syuh"], dir_name + "/q_" + str(args.download) + ".htm")
+    if parser.data[args.download-1]["touh"] != "not yet":
+        result = urllib.request.urlretrieve(root_html + parser.data[args.download-1]["touh"], dir_name + "/a_" + str(args.download) + ".htm")
+    print(parser.data[args.download-1]["title"])
+    print(root_html + parser.data[args.download-1]["syuh"])
+    print(root_html + parser.data[args.download-1]["touh"])
 
 elif args.done_Q:
-	LogDone("sanin_q",args.done_Q)
+    LogDone("sanin_q",args.done_Q)
 
 elif args.done_A:
-	LogDone("sanin_a",args.done_A)
+    LogDone("sanin_a",args.done_A)
 
 
 elif args.ulist:
-	UndoneList("sanin_q",parser)
-	UndoneList("sanin_a",parser)
+    UndoneList("sanin_q",parser)
+    UndoneList("sanin_a",parser)
